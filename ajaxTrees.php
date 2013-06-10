e28646cc0e91f47210e5241af313713399656be4
Klaus Jaschan <zhaokenliu@gmail.com>
2013-06-10 17:41:30 -0400

    index ahora lee base de datos para crear links
    aun falta tema del query string sea leido despues en el cloudinator.html
    tbn falta crear un grafo nuevo y clonar uno ya hecho.

---
+++ ajaxTrees.php
@@ -0,0 +1,18 @@
+<?php
+require_once('JSON.php');
+require_once('db.php');
+
+$json = new Services_JSON();
+
+try {	
+	$query = 'SELECT * FROM trees';
+	$datos = DBquery2($query);
+	$salida = $json->encode($datos);
+
+} catch (Exception $e) {
+	print('ERROR! '.$e);
+}
+
+print($salida);
+
+?> 
