12e006af1c9b58d9ea052af3a7cbd43cae8f80fc
Klaus Jaschan <zhaokenliu@gmail.com>
2013-06-11 01:44:24 -0400

    aun hay una pifia...
    ajaxTrees.php no envia de vuelta en el post el id del grafo recien
    creado

--- ajaxTrees.php
+++ ajaxTrees.php
@@ -4,15 +4,28 @@ require_once('db.php');
 
 $json = new Services_JSON();
 
-try {	
-	$query = 'SELECT * FROM trees';
-	$datos = DBquery2($query);
-	$salida = $json->encode($datos);
+if (isset($_POST['name'])) {
+	try {
+		DBquery3("INSERT INTO `cloudinator`.`trees` (`id`, `name`, `deleted`, `created`) VALUES 
+			(NULL, '".$_POST['name']."', 0, '".date("Y-m-d H:i:s")."');
+			");
 
-} catch (Exception $e) {
-	print('ERROR! '.$e);
+		$data = DBquery4("SELECT id FROM trees WHERE name='".$_POST['name']."'");
+		
+		print($json->encode($data));
+	} catch (Exception $e) {
+		print($e);
+	}
+}else{
+	try {	
+		$query = 'SELECT * FROM trees';
+		$datos = DBquery2($query);
+		$salida = $json->encode($datos);
+
+	} catch (Exception $e) {
+		print('ERROR! '.$e);
+	}
+
+	print($salida);
 }
-
-print($salida);
-
 ?> 
