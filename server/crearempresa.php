20dad20ba41375c8a6bd11d8ded7ca65d6e4f50b
Klaus Jaschan <klaus@jaschan.cl>
2013-10-28 19:40:07 -0300

    fix CLOUD-95, nombres repetidos de empresas not longer posible

--- server/crearempresa.php
+++ server/crearempresa.php
@@ -11,22 +11,31 @@ try {
 	$textarea = mysql_real_escape_string($_POST["textarea"]);
 	$modified = time();
 	
-	$insertempresa = "INSERT INTO `empresas` (`id`, `nombre`, `industria`, `contactado`, `areacontacto`, `infolevantamiento`, `modified`) VALUES 
-				(NULL, '$name', '$industry', '', '', '$textarea', $modified );";
-	DBQuery($insertempresa);
-
-	$nueva = DBQuery("SELECT * FROM empresas WHERE nombre = '$name'");
-	$response = $nueva->fetch_array(MYSQLI_ASSOC);
-	$data = array(
-				'result' => true,
-				'id'=>$response['id']
+	$existing = DBQuery("SELECT * FROM empresas WHERE nombre = '$name'");
+	if($existing->num_rows > 0){
+		$data = array(
+				'result' => false,
+				'exception'=> 'existing'
 			);
+	}else{
+		$insertempresa = "INSERT INTO `empresas` (`id`, `nombre`, `industria`, `contactado`, `areacontacto`, `infolevantamiento`, `modified`) VALUES 
+				(NULL, '$name', '$industry', '', '', '$textarea', $modified );";
+		DBQuery($insertempresa);
+
+		$nueva = DBQuery("SELECT * FROM empresas WHERE nombre = '$name'");
+		$response = $nueva->fetch_array(MYSQLI_ASSOC);
+		$data = array(
+					'result' => true,
+					'id' => $response['id']
+				);
+	}
 
 	print($json->encode($data));
 
 }catch (Exception $e) {
 	$data = array(
-				'result' => false
+				'result' => false,
+				'exception' => $e
 			);
 	print($json->encode($data));
 }
